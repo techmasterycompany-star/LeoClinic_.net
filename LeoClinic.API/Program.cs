e9956482c25f37c3e585473edb8ae808034844b4
Yousef sheha <sheha8757@gmail.com>
2026-08-02 00:42:15 +0300

    Initial Clean Architecture Setup

---
+++ LeoClinic.API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Readme.md
@@ -0,0 +1,24 @@
+# LeoClinic
+
+## Architecture
+
+- Clean Architecture
+
+## Technologies
+
+- .NET 9
+- SQL Server
+- EF Core
+- Repository Pattern
+- Swagger
+- FluentValidation
+- AutoMapper
+- JWT
+
+## Branches
+
+main
+
+dev
+
+feature/*
