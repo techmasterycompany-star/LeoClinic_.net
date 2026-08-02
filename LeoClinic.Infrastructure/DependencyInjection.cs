331011fe7d071a7979ac8c2bf0f0615e2b89ee48
Yousef sheha <sheha8757@gmail.com>
2026-08-02 06:21:01 +0300

    feat: Project-Setup & Configurations

--- LeoClinic.Infrastructure/DependencyInjection.cs
+++ LeoClinic.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,3 @@
-using LeoClinic.Domain.Entities;
 using LeoClinic.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -12,15 +11,6 @@ namespace LeoClinic.Infrastructure
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentityCore<User>(options =>
-            {
-                options.Password.RequireDigit = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequiredLength = 8;
-                options.User.RequireUniqueEmail = true;
-            })
-            .AddEntityFrameworkStores<AppDbContext>();
 
             return services;
         }
--- LeoClinic.Infrastructure/Migrations/20260802030504_InitialCreate.Designer.cs
+++ LeoClinic.Infrastructure/Migrations/20260802030504_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeoClinic.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260802015119_InitialCreate")]
+    [Migration("20260802030504_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
