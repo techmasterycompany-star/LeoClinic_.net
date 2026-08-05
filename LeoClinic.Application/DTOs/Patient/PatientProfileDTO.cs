389b2010fbf41728d6579c43fb8f5db43ffdb622
Radwa Mohamed <radwasedeek2013@gmail.com>
2026-08-05 22:40:37 +0300

    feat: manage patient profile

---
+++ LeoClinic.Application/DTOs/Patient/PatientProfileDTO.cs
@@ -0,0 +1,25 @@
+﻿using LeoClinic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoClinic.Application.DTOs.Patient
+{
+    public class PatientProfileDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.Patient;
+        public bool IsBlocked { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public bool IsApproved { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ LeoClinic.Application/Interfaces/IPatientRepository.cs
@@ -0,0 +1,20 @@
+﻿using LeoClinic.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoClinic.Application.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<List<PatientProfile>> GetAllPatientsAsync();
+        Task<List<PatientProfile>> GetApprovedPatientsAsync();
+        Task<PatientProfile?> GetPatientByIdAsync(int id);
+        Task<PatientProfile> CreatePatientAsync(PatientProfile patientProfile);
+        Task UpdatePatientProfileAsync(PatientProfile patientProfile);
+        Task DeletePatientAsync(int id);
+        Task ApprovePatientAsync(int id);
+    }
+}
