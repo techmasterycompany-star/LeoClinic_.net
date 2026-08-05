0830464ffeb79cfbecb42078abb9fdea07d04970
Radwa Mohamed <radwasedeek2013@gmail.com>
2026-08-05 22:40:37 +0300

    feat: manage patient profile

---
+++ LeoClinic.Application/DTOs/Patient/UpdatePatientDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeoClinic.Application.DTOs.Patient
+{
+    public class UpdatePatientProfileDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string ContactNumber { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
+    }
+}
