367a55741d8610d8082a101245796af4948ff16e
Radwa Mohamed <radwasedeek2013@gmail.com>
2026-08-11 06:09:24 +0300

    fix: add validation for create appointment + update patient

--- LeoClinic.Application/DTOs/Patient/CreateAppointmentDTO.cs
+++ LeoClinic.Application/DTOs/Patient/CreateAppointmentDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace LeoClinic.Application.DTOs.Patient
 {
     public class CreateAppointmentDTO
     {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Zero and negative numbers are invalid for id")]
         public int DoctorId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Zero and negative numbers are invalid for id")]
         public int AvailabilityId { get; set; }
-        public string Notes { get; set; } = string.Empty;
+        public string? Notes { get; set; } = string.Empty;
     }
 }
--- LeoClinic.Application/DTOs/Patient/UpdatePatientDTO.cs
+++ LeoClinic.Application/DTOs/Patient/UpdatePatientDTO.cs
@@ -21,6 +21,7 @@ namespace LeoClinic.Application.DTOs.Patient
         [Required]
         [RegularExpression(@"^01[0-9]{9}$", ErrorMessage = "Contact number must be an 11-digit phone number starting with 01 (e.g. 01229494438).")]
         public string ContactNumber { get; set; } = string.Empty;
+        [Required]
         public DateTime DateOfBirth { get; set; }
         [Required]
         [MaxLength(100)]
